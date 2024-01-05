80d170fcaeaee0acf53294a3def952ad35a10bd5
1F616EMO <root@1f616emo.xyz>
2024-01-05 22:09:22 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# conensed_cobble
+# Condensed Cobblestones
+
+Add VERY CONDENSED version of cobblestone. 81 in one. This mod is an extract of [LinuxForks's fork of moreblocks](https://git.bananach.space/moreblocks.git).
---
+++ init.lua
@@ -0,0 +1,60 @@
+-- condensed_cobble/init.lua
+-- VERY CONDENSED version of cobblestone
+--[[
+    Copyright © 2011-2024 Hugo Locurcio and contributors.
+    Licensed under the zlib license. See LICENSE.md for more information.
+]]
+
+local S = minetest.get_translator("condensed_cobble")
+
+minetest.register_node("condensed_cobble:condensed_cobble", {
+    description = S("Condensed Cobblestone"),
+    tiles = { "moreblocks_cobble_compressed.png^[colorize:black:255]" },
+    is_ground_content = false,
+    groups = { cracky = 1, stone = 2 },
+    stack_max = 999,
+    sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_alias("moreblocks:condensed_cobble", "condensed_cobble:condensed_cobble")
+
+local COMPRESSED = "moreblocks:cobble_compressed"
+
+minetest.register_craft({
+    output = "moreblocks:stone_tile 324",
+    recipe = {
+        { COMPRESSED, COMPRESSED },
+        { COMPRESSED, COMPRESSED },
+    }
+})
+
+minetest.register_craft({
+    output = COMPRESSED,
+    recipe = {
+        { COMPRESSED, COMPRESSED, COMPRESSED },
+        { COMPRESSED, COMPRESSED, COMPRESSED },
+        { COMPRESSED, COMPRESSED, COMPRESSED }
+    }
+})
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "moreblocks:cobble_compressed 9",
+    recipe = { COMPRESSED }
+})
+
+minetest.register_craft({
+    type = "cooking",
+    cooktime = 45,  -- 5*9
+    output = "default:stone 81",
+    recipe = "moreblocks:cobble_condensed"
+})
+
+if minetest.get_modpath("technic") then
+    minetest.register_craft({
+        type = "grinding",
+        time = 27,  -- 3*9
+        output = "default:cobble 81",
+        recipe = "moreblocks:cobble_condensed"
+    })
+end
